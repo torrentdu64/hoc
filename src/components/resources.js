272d1b6fa3c66180eab40ac0a993d9c1ea89a0c0
torrent benoit <torrentdu64@gmail.com>
2017-10-10 17:20:18 +0200

    create resources component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+     super secret  React recipe
+       <ul>
+         <li>1 cup sugar</li>
+         <li>1 cup sel</li>
+         <li>2 glases</li>
+       </ul>
+    </div>
+    );
+}
