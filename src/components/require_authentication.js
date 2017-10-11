337ab67a9a9fab9cec3b09048f4c9d5ccdcd1153
torrent benoit <torrentdu64@gmail.com>
2017-10-11 16:45:41 +0200

    permission to resources

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -4,8 +4,20 @@ import {connect} from 'react-redux';
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
-      static contextTypes = {
-          router: React.proptypes.object;
+       static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+      componentWillMount() {
+        if (!this.props.authenticated){
+        this.context.router.push('/');
+        }
+      }
+
+      componentWillUpdate(nextProps) {
+          if (!nextProps.authenticated){
+            this.context.router.push('/');
+          }
       }
 
       render() {
